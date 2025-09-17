82517071262842927f87aecbfde6b0d0743368ac
Niel <dyprih@gmail.com>
2025-09-17 22:37:09 +0200

    radix sort implemented

--- ps_algo.c
+++ ps_algo.c
@@ -6,7 +6,7 @@
 /*   By: dprikhod <dprikhod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 16:26:04 by dprikhod          #+#    #+#             */
-/*   Updated: 2025/08/22 17:28:16 by dprikhod         ###   ########.fr       */
+/*   Updated: 2025/09/17 22:36:20 by dprikhod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,4 +21,73 @@ int	ft_is_sorted(t_stack *stack)
 		stack = stack->next;
 	}
 	return (1);
+}
+
+static void	ft_sort_3(t_stack **a)
+{
+	while (!ft_is_sorted(*a))
+	{
+		if ((*a)->value > (*a)->next->value)
+			sa(a);
+		else
+			rra(a);
+	}
+}
+
+void	ft_sort_small(t_stack **a, t_stack **b)
+{
+	int	size;
+
+	size = ft_stack_size(*a);
+	if (size <= 1 || ft_is_sorted(*a))
+		return ;
+	if (size == 2)
+		sa(a);
+	else if (size == 3)
+		ft_sort_3(a);
+	else
+	{
+		while (!ft_is_sorted(*a))
+		{
+			if ((*a)->value > (*a)->next->value)
+				sa(a);
+			else if (ft_stack_size(*b) < 2)
+				pb(a, b);
+			else
+			{
+				pa(a, b);
+				if ((*a)->value > (*a)->next->value)
+					sa(a);
+			}
+		}
+	}
+}
+
+// static int	sort_bit(t_stack **a, t_stack **b, int size)
+
+void	ft_radix_sort(t_stack **a, t_stack **b, int size)
+{
+	int	i;
+	int	j;
+	int	bit_size;
+
+	if (!a || !b || !*a || !*b || size < 6)
+		return ;
+	bit_size = 0;
+	while ((size - 1) >> bit_size)
+		bit_size++;
+	i = 0;
+	while (i++ < (bit_size + 1))
+	{
+		j = 0;
+		while (j++ < (size))
+		{
+			if ((((*a)->value >> (i - 1)) & 1) == 0)
+				pb(a, b);
+			else
+				ra(a);
+		}
+		while (*b)
+			pa(a, b);
+	}
 }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: dprikhod <dprikhod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/14 20:29:12 by dprikhod          #+#    #+#             */
-/*   Updated: 2025/08/22 17:04:39 by dprikhod         ###   ########.fr       */
+/*   Updated: 2025/09/17 22:09:25 by dprikhod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,14 +50,16 @@ t_hash_node				**ft_hash_init(void);
 void					ft_hash_clear(t_hash_node **table);
 int						ft_stack_size(t_stack *stack);
 int						ft_is_sorted(t_stack *stack);
+void					ft_sort_small(t_stack **a, t_stack **b);
+void					ft_radix_sort(t_stack **a, t_stack **b, int size);
 
-void	sa(t_stack **a);
-void	sb(t_stack **b);
-void	rb(t_stack **b);
-void	ra(t_stack **a);
-void	rra(t_stack **a);
-void	rrb(t_stack **b);
-void	pb(t_stack **a, t_stack **b);
-void	pa(t_stack **b, t_stack **a);
+void					sa(t_stack **a);
+void					sb(t_stack **b);
+void					rb(t_stack **b);
+void					ra(t_stack **a);
+void					rra(t_stack **a);
+void					rrb(t_stack **b);
+void					pb(t_stack **a, t_stack **b);
+void					pa(t_stack **b, t_stack **a);
 
 #endif
